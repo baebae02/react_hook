556ac0e7ae66c8d1f7ede39b19859e4b853e851d
Seo Hyeon Bae <bae4614@gmail.com>
2022-08-16 00:47:55 +0900

    feat: userTab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import React from "react";
-import {useInput} from "./userInput";
+import {useTabs} from "./userHooks/userTab";
+
+const content = [
+    {
+        tab: "Section 1",
+        content: "I'm the content of the Section 1"
+    },
+    {
+        tab: "Section 2",
+        content: "I'm the content of the Section 2"
+    }
+]
 
 const App = () => {
-    const maxLen = value => !value.includes("@");
-    const name = useInput('bae', maxLen);
+    const { currentItem, changeItem } = useTabs(0, content);
     return (
         <div className="App">
             <p>Hello</p>
-            <input placeholder="name" {...name} />
+            {content.map((section, index) => (
+                <button onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            <div>{currentItem.content}</div>
         </div>
     );
 }
