8fe7f1c5c8d0daa75ea27d9a8fc3a47b41acefb3
Seo Hyeon Bae <bae4614@gmail.com>
2022-08-16 11:06:54 +0900

    feat: usePreventLeave

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from "react";
 
+
 const App = () => {
     return (
         <div className="App">
-            <button>Delete the World!</button>
+
         </div>
     );
 }
