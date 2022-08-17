3c4f246db08898972a30b3fd8d8616d9520560eb
Seo Hyeon Bae <bae4614@gmail.com>
2022-08-17 23:46:03 +0900

    feat: useFullScreen (without callback)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import {useScroll} from "./userHooks/useScroll";
+import {useRef} from "react";
 
+const useFullscreen = () => {
+    const element = useRef();
+    const triggerFull = () => {
+        if (element.current) {
+            element.current.requestFullscreen();
+        }
+    }
+    const exitFull = () => {
+        document.exitFullscreen();
+    }
+    return {element, triggerFull, exitFull};
+}
 const App = () => {
-    const {y} = useScroll();
+    const {element, triggerFull, exitFull} = useFullscreen();
     return (
         <div className="App" style={{ height: "1000vh"}}>
-            <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+            <div ref={element}>
+                <img
+                    src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAyMjhfMTE5%2FMDAxNjQ2MDU4NzUyMDMw.JQYQJTbUCUIBzvxgvi6ABRZV7TJD-CuBATDKFgbpYxgg.kmbsip7Kk34GE9ipvONn0v_RYttfweVNbqnoGjJ67nEg.JPEG.ahk1226%2FIMG_2630.JPG&type=sc960_832"
+                    height="500px"
+                    alt=""
+                />
+                <button onClick={exitFull}>Exit Full</button>
+            </div>
+            <button onClick={triggerFull}>Full Screen</button>
         </div>
     );
 };
