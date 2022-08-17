c68913a62f1aad734bb3ef2a3bfd7da914f7e930
Seo Hyeon Bae <bae4614@gmail.com>
2022-08-18 00:27:01 +0900

    feat: useNotification

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import {useFullscreen} from "./userHooks/useFullscreen";
+import {useNotification} from "./userHooks/useNotification";
 
 const App = () => {
-    const callback = (isFull) => {
-        console.log(isFull ? "We are Full" : "We are Small");
-    }
-    const {element, triggerFull, exitFull} = useFullscreen(callback);
+    const triggerNoti = useNotification("can I change your noti?", { body: "It's me"
+    });
     return (
         <div className="App" style={{ height: "1000vh"}}>
-            <div ref={element}>
-                <img
-                    src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAyMjhfMTE5%2FMDAxNjQ2MDU4NzUyMDMw.JQYQJTbUCUIBzvxgvi6ABRZV7TJD-CuBATDKFgbpYxgg.kmbsip7Kk34GE9ipvONn0v_RYttfweVNbqnoGjJ67nEg.JPEG.ahk1226%2FIMG_2630.JPG&type=sc960_832"
-                    height="500px"
-                    alt=""
-                />
-                <button onClick={exitFull}>Exit Full</button>
-            </div>
-            <button onClick={triggerFull}>Full Screen</button>
+            <button onClick={triggerNoti}>Hello</button>
         </div>
     );
 };
